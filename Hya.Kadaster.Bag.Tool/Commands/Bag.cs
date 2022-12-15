bc2e78712fffd366497dbeb861f335d9e17bce27
Mark Hijdra <mark.hijdra@iodigital.com>
2022-12-15 13:15:24 +0100

    Description update

--- Hya.Kadaster.Bag.Tool/Commands/Bag.cs
+++ Hya.Kadaster.Bag.Tool/Commands/Bag.cs
@@ -4,9 +4,11 @@ namespace Hya.Kadaster.Bag.Tool.Commands;
 
 public class Bag : RootCommand
 {
-    public Bag() : base("dotnet run bag -h")
+    public Bag() : base("Cli tool for querying data from Kadaster Bag API")
     {
         AddCommand(new AddressFind());
         AddCommand(new AddressGet());
     }
+
+    public override string Name => "bag";
 }
